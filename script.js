7471216991206c140fba967a905a2b4ecfbfc1c4
nivicius <niviciusdev@gmail.com>
2024-01-12 03:22:47 -0300

    implementing script

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const inputBox = document.getElementById("input-box");
+const inputBox_day = document.getElementById("input-box_day");
+const inputBox_night = document.getElementById("input-box_night");
+const listContainer_day= document.getElementById("list-container_day");
+const listContainer_night = document.getElementById("list-container_night");
+
+function addTask_day() {
+  if (inputBox_day.value === "") {
+    alert("Por favor, insira uma tarefa!");
+    return;
+  }
+  else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox_day.value;
+    listContainer_day.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox_day.value = "";
+}
